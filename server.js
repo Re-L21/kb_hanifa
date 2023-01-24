b1e28cb026a00abe6775063a27249ef80d9c1c00
Re-L21 <gabrielokto21@gmail.com>
2023-01-24 20:31:44 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+app.set("view engine", "ejs")
+app.set("views", "views")
+app.use(express.static(__dirname + '/views/utility'));
+
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "kb_hanifa",
+    user: "root",
+    password: "",
+})
+
+db.connect((err) => {
+
+    if (err) throw err;
+    console.log("db conect");
+
+    app.get("/", (req, res) => {
+        const home = "SELECT * FROM home";
+        db.query(home, (err, result) => {
+            
+            const data_home = JSON.parse(JSON.stringify(result));
+            res.render("index", { data_home: data_home });
+
+        })
+    })
+
+
+})
+
+
+app.listen(8000, () => {
+    console.log("ready");
+})
